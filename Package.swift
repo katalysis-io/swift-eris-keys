3356e1598621f1c61d021312f5039d1ac4184da9
Alex Tran Qui <alex@katalysis.io>
2017-10-21 10:12:57 +0200

    move to swift 4.0

--- Package.swift
+++ Package.swift
@@ -1,9 +1,18 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
   name: "ErisKeys",
+  products: [
+    .library(name: "ErisKeys", targets: ["ErisKeys"])
+    ],
   dependencies: [
-    .Package(url: "https://gitlab.com/katalysis/Ed25519.git", majorVersion: 0, minor: 1),
-    .Package(url: "https://gitlab.com/katalysis/RipeMD.git", majorVersion: 0, minor: 1),
+    .package(url: "https://gitlab.com/katalysis/Ed25519.git", .branch("swift-4")),
+    .package(url: "https://gitlab.com/katalysis/RipeMD.git", from: "0.1.0"),
+    ],
+  targets: [
+    .target(name: "ErisKeys", dependencies: ["Ed25519", "RipeMD"], path: ".", sources: ["Sources"]),
     ]
+
 )
