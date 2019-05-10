bf2175330739abe82d0da14b612cd1e574238d73
Ilya Karpuk <ilya@katalysis.io>
2019-05-10 11:15:07 +0200

    Ed25519 0.2.0 -> 0.2.2

--- Package.swift
+++ Package.swift
@@ -6,12 +6,12 @@ let package = Package(
   name: "ErisKeys",
   products: [
     .library(name: "ErisKeys", targets: ["ErisKeys"])
-    ],
+  ],
   dependencies: [
-    .package(url: "https://gitlab.com/katalysis/open-source/Ed25519.git", from: "0.2.0"),
+    .package(url: "https://gitlab.com/katalysis/open-source/Ed25519.git", from: "0.2.2"),
     .package(url: "https://gitlab.com/katalysis/open-source/RipeMD.git", from: "0.1.5"),
-    ],
+  ],
   targets: [
     .target(name: "ErisKeys", dependencies: ["Ed25519", "RipeMD"], path: ".", sources: ["Sources"]),
-    ]
+  ]
 )
