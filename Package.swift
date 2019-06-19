da45c761afeb4ac770c0a98b4150c260d229fd57
Ilya Karpuk <ilya@katalysis.io>
2019-06-19 15:14:07 +0200

    Updated to swift 5, ED25519 - 0.3.1

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     .library(name: "ErisKeys", targets: ["ErisKeys"])
   ],
   dependencies: [
-    .package(url: "https://gitlab.com/katalysis/open-source/Ed25519.git", from: "0.2.2"),
+    .package(url: "https://gitlab.com/katalysis/open-source/Ed25519.git", from: "0.3.1"),
     .package(url: "https://gitlab.com/katalysis/open-source/RipeMD.git", from: "0.1.5"),
   ],
   targets: [
