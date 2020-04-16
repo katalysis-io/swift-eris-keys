867b11e43a1e8c14eccd9fb496397114395151b9
Alex Tran Qui <alex@katalysis.io>
2020-04-16 15:46:27 +0200

    Update to swift 5.2, min target oses

--- Package.swift
+++ Package.swift
@@ -1,9 +1,15 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.2
 
 import PackageDescription
 
 let package = Package(
   name: "ErisKeys",
+  platforms: [
+      .macOS(.v10_15),
+      .iOS(.v13),
+      .watchOS(.v6),
+      .tvOS(.v13),
+ ],
   products: [
     .library(name: "ErisKeys", targets: ["ErisKeys"])
     ],
@@ -12,6 +18,7 @@ let package = Package(
     .package(url: "https://gitlab.com/katalysis/open-source/RipeMD.git", from: "0.1.5"),
     ],
   targets: [
-    .target(name: "ErisKeys", dependencies: ["Ed25519", "RipeMD"], path: ".", sources: ["Sources"]),
+    .target(name: "ErisKeys", dependencies: ["Ed25519", "RipeMD"]),
+    .testTarget(name: "ErisKeysTests", dependencies: ["Ed25519", "RipeMD"]),
     ]
 )
--- Tests/ErisKeysTests/ErisKeysTests.swift
+++ Tests/ErisKeysTests/ErisKeysTests.swift
@@ -2,16 +2,36 @@ import XCTest
 @testable import ErisKeys
 
 class ErisKeysTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(ErisKeys().text, "Hello, World!")
+    func testSignedMessage() {
+        
+        guard let key = ErisKey(seed: "744D57D112D795F2C4CEBA22EEC09C9DC16FAFB0E7F737AB3CB50EC92F9C6581".toByteArray()!) else {
+            XCTAssert(false, "Problems creating key")
+            return
+        }
+        let pubKey: [UInt8] = "A940E6820B6CB783575B20F0109AF934726C521A12DDDB3D822141402C39C850C41510D45A95A8F9DC52309C47E113FE79595CCDC54F8E6F0137DDA68EEA0177".toByteArray()!
+        let account = ErisKey.account(pubKey)
+        XCTAssertEqual(account, key.account)
+        XCTAssertEqual(pubKey, key.pubKey)
+        let msgArray: [UInt8] = Array("""
+{"content":"e4c86e8e3a76434729b1bd03cd588765d11d2bb15e9b5f4408ff375c13c65dd0","participant":"A940E6820B6CB783575B20F0109AF934726C521A12DDDB3D822141402C39C850C41510D45A95A8F9DC52309C47E113FE79595CCDC54F8E6F0137DDA68EEA0177"}
+""".utf8)
+        
+        let s = key.sign(msgArray)
+        print(s.reduce("", { (r, u) -> String in
+            return r + String(format: "%02X", u)
+        }))
+        
+        let signed: [UInt8] = "26B0BB48F137FEA5732E2E42A685CEF42418FAEDB3C354036915D4B421318160560C0A00820DE282A75C91E06001742981BFB34FEEBF3603E16A3DE923D8786700".toByteArray()!
+        
+        
+        XCTAssertTrue(ErisKey.verify(pubKey, msgArray, s))
+        XCTAssertTrue(ErisKey.verify(pubKey, msgArray, signed))
     }
 
 
     static var allTests : [(String, (ErisKeysTests) -> () throws -> Void)] {
         return [
-            ("testExample", testExample),
+            ("testExample", testSignedMessage),
         ]
     }
 }
